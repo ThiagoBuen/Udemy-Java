1c20dcba1c6f91d1078f690090c25d98786682a8
ThiagoBuen <thiagobueno@gea.inatel.br>
2022-11-08 20:34:18 -0300

    08/11 - Datetime operations, convertions and so

--- Section11/DataTime/src/app/Main.java
+++ Section11/DataTime/src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -50,5 +51,48 @@ public class Main {
         System.out.println("data05 formatted: " + fmt4.format(date05));
         System.out.println("data06 formatted: " + fmt5.format(date06));
         System.out.println("data06 formatted: " + date06.toString());
+
+        //DateTime Operations
+
+        LocalDate r01 = LocalDate.ofInstant(date06, ZoneId.systemDefault());
+        LocalDate r02 = LocalDate.ofInstant(date06, ZoneId.of("Portugal"));
+        LocalDateTime r03 = LocalDateTime.ofInstant(date06, ZoneId.systemDefault());
+        LocalDateTime r04 = LocalDateTime.ofInstant(date06, ZoneId.of("Portugal"));
+
+
+        System.out.println("r01: " + r01);
+        System.out.println("r02: " + r02);
+        System.out.println("r03: " + r03);
+        System.out.println("r04: " + r04);
+
+        System.out.println("d04 day: " + date04.getDayOfMonth());
+        System.out.println("d04 month: " + date04.getMonthValue());
+        System.out.println("d04 year: " + date04.getYear());
+
+        LocalDate pastWeekLocalDate = date04.minusWeeks(1);
+        LocalDate nextWeekLocalDate = date04.plusDays(7);
+
+        System.out.println("pastWeekLocalDate: " + pastWeekLocalDate);
+        System.out.println("nextWeekLocalDate: " + nextWeekLocalDate);
+
+        LocalDateTime pastYearLocalDateTime = date05.minusYears(2);
+        LocalDateTime nextYearLocalDateTime = date05.plusYears(2);
+
+        System.out.println("pastYearLocalDateTime: " + pastYearLocalDateTime);
+        System.out.println("nextYearLocalDateTime: " + nextYearLocalDateTime);
+
+        Instant pastWeekInstant = date06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = date06.plus(7, ChronoUnit.DAYS);
+
+        System.out.println("pastWeekInstant: " + pastWeekInstant);
+        System.out.println("nextWeekInstant: " + nextWeekInstant);
+
+        Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), date04.atStartOfDay());
+        Duration t2 = Duration.between(pastYearLocalDateTime, date05);
+        Duration t3 = Duration.between(date06, pastWeekInstant);
+        System.out.println("t1: " + t1.toDays());
+        System.out.println("t2: " + t2.toDays());
+        System.out.println("t3: " + t3.toDays());
+
     }
 }
