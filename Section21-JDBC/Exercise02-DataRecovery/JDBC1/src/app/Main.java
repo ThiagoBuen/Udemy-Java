0618b09294d215d87ac87f9b54b0a6340da1dc70
ThiagoBuen <thiagobueno@gea.inatel.br>
2023-12-21 20:02:03 -0300

    Demo2 - DataRetrieval

---
+++ Section21-JDBC/Exercise02-DataRecovery/JDBC1/src/app/Main.java
@@ -0,0 +1,37 @@
+package app;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.getStackTrace();
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+}
